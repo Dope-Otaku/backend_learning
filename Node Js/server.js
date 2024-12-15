b6de8647182fa95abfe0699ff5faa90e5e5b9e56
Dope-Otaku <fakeyt909@gmail.com>
2024-12-15 23:55:58 +0530

    created a seperate folder for route handling

--- Node Js/server.js
+++ Node Js/server.js
@@ -5,6 +5,12 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 
+app.get("/api/contacts", (req, res) =>{
+    res.status(200).json({"message": "this api will fetch Contacts list"})
+})
+
+
+
 app.listen(port, ()=>{
     console.log(`server running on port: ${port}`)
 })
